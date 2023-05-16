0144547f54b04adcef1b202f2884d2d0775d6baf
Prasad Dumbre <prasaddumbre@outlook.com>
2023-05-16 20:05:21 +0530

    Add navigation stack with 2 screens

--- App.tsx
+++ App.tsx
@@ -6,6 +6,9 @@
  */
 
 import React from 'react';
+
+import RootNavigatonStack from './src/navigation/RootNavigatonStack';
+
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -62,38 +65,7 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return <RootNavigatonStack />;
 }
 
 const styles = StyleSheet.create({
---
+++ src/navigation/RootNavigatonStack.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import UsersList from '../screens/UsersList';
+import UserDetails from '../screens/UserDetails';
+
+const Stack = createStackNavigator();
+
+const RootNavigatonStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="UsersList" component={UsersList} />
+        <Stack.Screen name="UserDetails" component={UserDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigatonStack;
